a451fa53b91ddcf38b33a7d6c102f274ae6fcce6
rayray39 <raynerlimfy@gmail.com>
2025-01-05 13:23:54 +0800

    Add form to modal for adding item

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Form from './Form'
+import ModalForm from './ModalForm'
 import { Button } from 'react-bootstrap';
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     return <>
         <Button variant='light' size='lg' onClick={() => setShowForm(true)}>Add Item</Button>
 
-        <Form show={showForm} onHide={() => setShowForm(false)} />
+        <ModalForm show={showForm} onHide={() => setShowForm(false)} />
     </>
 }
 
---
+++ src/ModalForm.jsx
@@ -0,0 +1,63 @@
+import { Modal } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import { Form } from 'react-bootstrap';
+import { InputGroup } from 'react-bootstrap';
+
+function ModalForm(props) {
+    // use react-bootstrap's modal
+    return <>
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+            >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Add a new item
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div>
+                    <FillItems />
+                </div>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant='dark' onClick={props.onHide}>Confirm</Button>
+            </Modal.Footer>
+        </Modal>
+    </>
+}
+
+function FillItems() {
+    // form within the modal to fill up information about item added.
+    return <>
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicText">
+                <Form.Label>Description</Form.Label>
+                <Form.Control type="text" placeholder="Enter description" />
+            </Form.Group>
+
+            <InputGroup className="mb-3">
+                <InputGroup.Text>$</InputGroup.Text>
+                <Form.Control aria-label="Amount (to the nearest dollar)" />
+            </InputGroup>
+
+            <Form.Select style={{marginTop:'15px'}} aria-label="select module">
+                <option>Select module</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </Form.Select>
+
+            <Form.Select aria-label="select category">
+                <option>Select category</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </Form.Select>
+        </Form>
+    </>
+}
+
+export default ModalForm;
