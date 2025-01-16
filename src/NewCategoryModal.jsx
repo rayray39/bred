ba550e2555da07eba7983be8430b8287a2e8d8b3
rayray39 <raynerlimfy@gmail.com>
2025-01-16 11:04:31 +0800

    Add onChange and onClick handlers

--- src/NewCategoryModal.jsx
+++ src/NewCategoryModal.jsx
@@ -1,34 +1,47 @@
 import Modal from 'react-bootstrap/Modal';
 import { Button } from 'react-bootstrap';
 import { Form } from 'react-bootstrap';
+import { useState } from 'react';
 
 // modal for adding new category, will open when 'Add Category' button in MainCells is clicked
 function NewCategoryModal(props) {
-  return (
-    <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="new-category-modal-title-vcenter"
-        centered
-        >
-        <Modal.Header closeButton>
-            <Modal.Title id="new-category-modal-title-vcenter">
-            Add New Category
-            </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicText">
-                    <Form.Control type="text" placeholder="New Category" name='new-category' required />
-                    <Form.Control.Feedback type='invalid'>Please enter a category.</Form.Control.Feedback>
-                </Form.Group>
-            </Form>
-        </Modal.Body>
-        <Modal.Footer>
-            <Button variant='dark' onClick={props.onHide}>Confirm</Button>
-        </Modal.Footer>
-    </Modal>
-  );
+    const [newCategory, setNewCategory] = useState(null);
+    
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setNewCategory(value);
+    }
+
+    const handleConfirm = () => {
+        props.onHide();
+        console.log(newCategory);
+    }
+
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="new-category-modal-title-vcenter"
+            centered
+            >
+            <Modal.Header closeButton>
+                <Modal.Title id="new-category-modal-title-vcenter">
+                Add New Category
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicText">
+                        <Form.Control type="text" placeholder="New Category" name='new-category' required onChange={handleChange} />
+                        <Form.Control.Feedback type='invalid'>Please enter a category.</Form.Control.Feedback>
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant='dark' onClick={handleConfirm}>Confirm</Button>
+            </Modal.Footer>
+        </Modal>
+    );
 }
 
 export default NewCategoryModal
--- src/NewModuleModal.jsx
+++ src/NewModuleModal.jsx
@@ -1,34 +1,47 @@
 import Modal from 'react-bootstrap/Modal';
 import { Button } from 'react-bootstrap';
 import { Form } from 'react-bootstrap';
+import { useState } from 'react';
 
 // modal for adding new module, will open when 'Add Module' button in MainCells is clicked
 function NewModuleModal(props) {
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="new-module-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-            <Modal.Title id="new-module-modal-title-vcenter">
-            Add New Module
-            </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicText">
-                    <Form.Control type="text" placeholder="New Module" name='new-module' required />
-                    <Form.Control.Feedback type='invalid'>Please enter a module.</Form.Control.Feedback>
-                </Form.Group>
-            </Form>
-        </Modal.Body>
-        <Modal.Footer>
-            <Button variant='dark' onClick={props.onHide}>Confirm</Button>
-        </Modal.Footer>
-    </Modal>
-  );
+    const [newModule, setNewModule] = useState(null);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setNewModule(value);
+    }
+
+    const handleConfirm = () => {
+        props.onHide();
+        console.log(newModule);
+    }
+
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="new-module-modal-title-vcenter"
+            centered
+        >
+        <Modal.Header closeButton>
+                <Modal.Title id="new-module-modal-title-vcenter">
+                Add New Module
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicText">
+                        <Form.Control type="text" placeholder="New Module" name='new-module' required onChange={handleChange} />
+                        <Form.Control.Feedback type='invalid'>Please enter a module.</Form.Control.Feedback>
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant='dark' onClick={handleConfirm}>Confirm</Button>
+            </Modal.Footer>
+        </Modal>
+    );
 }
 
 export default NewModuleModal
