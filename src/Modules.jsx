1d533ffcc3c66bd7d9c47a14deb6da9d847635cd
rayray39 <raynerlimfy@gmail.com>
2025-01-16 10:25:23 +0800

    Update all modules to use useEffect hook

--- src/Modules.jsx
+++ src/Modules.jsx
@@ -22,48 +22,53 @@ const MenuProps = {
 
 // provides the select (dropdown) for Modules
 export default function Modules({handleSelectedModules}) {
-  const theme = useTheme();
-  const [moduleName, setModuleName] = React.useState([]);
+    const theme = useTheme();
+    const [moduleName, setModuleName] = React.useState([]);
+    const [allModules, setAllModules] = React.useState(MODULES);  // list of all modules
 
-  const handleChange = (event) => {
-    const value = event.target.value;   // value is always an array
-    console.log(`selected modules (Modules.jsx): ${value}`);
-    handleSelectedModules(value);
-    setModuleName(
-      // On autofill we get a stringified value.
-      // in the event that value is not an array (a string), split it to get an array.
-      typeof value === 'string' ? value.split(',') : value,
+    React.useEffect(() => {
+        setAllModules(MODULES);
+    }, [MODULES])
+
+    const handleChange = (event) => {
+        const value = event.target.value;   // value is always an array
+        console.log(`selected modules (Modules.jsx): ${value}`);
+        handleSelectedModules(value);
+        setModuleName(
+            // On autofill we get a stringified value.
+            // in the event that value is not an array (a string), split it to get an array.
+            typeof value === 'string' ? value.split(',') : value,
+        );
+    };
+
+    return (
+        <div>
+        <FormControl sx={{ mt: 2, width: 300, bgcolor: 'white', borderRadius:'4px'}} size='small'>
+            <InputLabel id="multiple-module-label" sx={{color:'black'}}>Module</InputLabel>
+            <Select
+                labelId="multiple-module-label"
+                id="multiple-module"
+                multiple
+                value={moduleName}
+                onChange={handleChange}
+                input={<OutlinedInput label="Module" />}
+                renderValue={(selected) => selected.join(', ')}
+                MenuProps={MenuProps}
+                sx={{ 
+                    color: 'black', // Set selected text color
+                    '.MuiSelect-icon': { color: 'black' }, // Set dropdown arrow color
+                }}
+                >
+                {allModules.map((module) => (
+                    <MenuItem key={module} value={module}>
+                        <Checkbox checked={moduleName.includes(module)} />
+                        <ListItemText primary={module} />
+                    </MenuItem>
+                ))}
+            </Select>
+        </FormControl>
+        </div>
     );
-  };
-
-  return (
-    <div>
-      <FormControl sx={{ mt: 2, width: 300, bgcolor: 'white', borderRadius:'4px'}} size='small'>
-        <InputLabel id="multiple-module-label" sx={{color:'black'}}>Module</InputLabel>
-        <Select
-          labelId="multiple-module-label"
-          id="multiple-module"
-          multiple
-          value={moduleName}
-          onChange={handleChange}
-          input={<OutlinedInput label="Module" />}
-          renderValue={(selected) => selected.join(', ')}
-          MenuProps={MenuProps}
-          sx={{ 
-            color: 'black', // Set selected text color
-            '.MuiSelect-icon': { color: 'black' }, // Set dropdown arrow color
-          }}
-        >
-          {MODULES.map((module) => (
-            <MenuItem key={module} value={module}>
-                <Checkbox checked={moduleName.includes(module)} />
-                <ListItemText primary={module} />
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </div>
-  );
 }
 
 
